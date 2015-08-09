dae60b617669ec5a7e08a7cc3d6b500f7a2e2217
Francisco Rodriguez <idtgame1403@gmail.com>
2015-08-09 10:29:52 -0500

    Update brag.py

--- brag.py
+++ brag.py
@@ -1,11 +1,20 @@
+#DEMO "BRAG!" — A BASIC program from the past.
+
+#declare blank strings
+
+firstname = ""
+lastname = ""
+answer = ""
+
+#inroduction
+
 print("Hello! I am a program written in Python!")
 print("I would like to show you a few things that a Python program can do!\n")
 print("First, I need some information.")
 print("Tell me your first name. Press ENTER after you type it.")
 print
 
-firstname = ""
-lastname = ""
+#ask for name, make sure there's an answer
 
 while firstname == "":
     firstname = input('Enter your first name: ')
@@ -17,27 +26,28 @@ while lastname == "":
 if lastname == "":
     print("Now, cut that out!")
 
+#piece answers together for full name and pull name length info
+
 wholename = firstname + " " + lastname
 lenfirst = len(firstname)
 lenlast = len(lastname)
 lenwhole = len(wholename)
+
+#count letters in name
 
 print("\nWell hello, " + wholename + "! That's a very nice name!")
 print
 print("Did you know that there are", lenfirst, "letters in your first name and", lenlast, "letters in your last name? That makes", lenfirst + lenlast, "letter in all!!!")
 print
 
-answer = ""
-
 print("\nWould you like to see your name printed backwards?")
 
-while answer[: 1] != "y":
+while answer[: 1] != "y" and answer[: 1] != "Y":
     answer = input('Type yes or no: ')
-
-if answer == "":
-    print("I need an answer, please.")
-if answer[: 1] == "n":
-    print("Please, let me show off for you!")
+    if answer[: 1] == "n" or answer[: 1] == "N":
+        print("Please, let me show off for you!")
+    if answer == "":
+        print("I need an answer, please.")
 
 i = lenwhole
 
@@ -47,8 +57,8 @@ print
 import time
 while i > 0:
     print(wholename[i - 1: i], end = "")
-i = i - 1
-time.sleep(.25)
+    i = i - 1
+    time.sleep(.25)
 
 print(end = '\n')
 
@@ -57,12 +67,26 @@ month = int(input('First, the number of the month (1-12): '))
 day = int(input('Next, the day: '))
 year = int(input('And finally, the year: '))
 
-if year > 1900:
-    year = year - 1900
 if year > 2000:
     year = year - 2000
+if year > 1900:
+    year = year - 1900
 
 y1 = 365 * (year - 1) + (year - 1) / 4 + 1
+
+if year % 4 != 0:
+    daysinmonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+else:
+    daysinmonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+d1 = y1
+
+for j in range(0, month - 1):
+    d1 = d1 + daysinmonth[j]
+
+d1 = d1 + day
+
+age = d1
 
 import time
 import datetime
@@ -73,10 +97,6 @@ yearnow = int(datetime.date.today().strftime("%Y"))
 monthnow = (datetime.date.today().strftime("%B"))
 daynow = (datetime.date.today().strftime("%d"))
 
-
-if year % 4 != 0:
-    daysinmonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-else :
-    daysinmonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-print(y1)
+print(monthnow)
+print(daynow)
+print(yearnow)
