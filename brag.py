6df9014d5bd8727022f9488bc8d50db734093330
Francisco Rodriguez <idtgame1403@gmail.com>
2015-08-08 21:07:39 -0500

    Update brag.py

--- brag.py
+++ brag.py
@@ -1,43 +1,43 @@
-print ("Hello! I am a program written in Python!")
-print ("I would like to show you a few things that a Python program can do!\n")
-print ("First, I need some information.")
-print ("Tell me your first name. Press ENTER after you type it.")
+print("Hello! I am a program written in Python!")
+print("I would like to show you a few things that a Python program can do!\n")
+print("First, I need some information.")
+print("Tell me your first name. Press ENTER after you type it.")
 print
 
 firstname = ""
 lastname = ""
 
 while firstname == "":
-    firstname = input ('Enter your first name: ')
-    if firstname == "":
-        print ("Now, cut that out!")
-    
+    firstname = input('Enter your first name: ')
+if firstname == "":
+    print("Now, cut that out!")
+
 while lastname == "":
-    lastname = input ('And your last name: ')
-    if lastname == "":
-        print ("Now, cut that out!")
+    lastname = input('And your last name: ')
+if lastname == "":
+    print("Now, cut that out!")
 
 wholename = firstname + " " + lastname
 lenfirst = len(firstname)
 lenlast = len(lastname)
 lenwhole = len(wholename)
 
-print ("\nWell hello, " + wholename + "! That's a very nice name!")
+print("\nWell hello, " + wholename + "! That's a very nice name!")
 print
-print ("Did you know that there are",lenfirst,"letters in your first name and",lenlast,"letters in your last name? That makes",lenfirst+lenlast,"letter in all!!!")
+print("Did you know that there are", lenfirst, "letters in your first name and", lenlast, "letters in your last name? That makes", lenfirst + lenlast, "letter in all!!!")
 print
 
 answer = ""
 
-print ("\nWould you like to see your name printed backwards?")
+print("\nWould you like to see your name printed backwards?")
 
-while answer[:1] != "y":
-    answer = input ('Type yes or no: ')
+while answer[: 1] != "y":
+    answer = input('Type yes or no: ')
 
-    if answer == "":
-        print ("I need an answer, please.")
-    if answer[:1] == "n":
-        print ("Please, let me show off for you!")
+if answer == "":
+    print("I need an answer, please.")
+if answer[: 1] == "n":
+    print("Please, let me show off for you!")
 
 i = lenwhole
 
@@ -46,13 +46,13 @@ print
 
 import time
 while i > 0:
-    print (wholename[i-1:i], end="")
-    i = i - 1
-    time.sleep(.25)
+    print(wholename[i - 1: i], end = "")
+i = i - 1
+time.sleep(.25)
 
-print (end = '\n')
+print(end = '\n')
 
-print ("\nNow, tell me your birthdate.")
+print("\nNow, tell me your birthdate.")
 month = int(input('First, the number of the month (1-12): '))
 day = int(input('Next, the day: '))
 year = int(input('And finally, the year: '))
@@ -67,7 +67,7 @@ y1 = 365 * (year - 1) + (year - 1) / 4 + 1
 import time
 import datetime
 
-print ("\nToday's date is ", )
+print("\nToday's date is ", )
 
 yearnow = int(datetime.date.today().strftime("%Y"))
 monthnow = (datetime.date.today().strftime("%B"))
@@ -75,8 +75,8 @@ daynow = (datetime.date.today().strftime("%d"))
 
 
 if year % 4 != 0:
-    daysinmonth = [31,29,31,30,31,30,31,31,30,31,30,31]
-else:
-    daysinmonth = [31,28,31,30,31,30,31,31,30,31,30,31]
+    daysinmonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+else :
+    daysinmonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-print (y1)
+print(y1)
